76c8b41d5323f368dc480d5f716027f93413fa3b
brunohseibt-bhs <bruno@seibtenterprises.com>
2026-03-05 16:57:45 -0500

    Initial commit

---
+++ generate_function.py
@@ -0,0 +1,18 @@
+def count_vowels(text):
+    vowels = "aeiouAEIOU"
+    count = 0
+
+    for char in text:
+        if char in vowels:
+            count += 1
+
+    return count
+
+
+# User input
+user_text = input("Enter a string: ")
+
+# Function call
+total_vowels = count_vowels(user_text)
+
+print("Number of vowels:", total_vowels)
