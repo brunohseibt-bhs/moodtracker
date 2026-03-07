73a1b623cdafc7ef85d8ac41d54dda6298de1a0b
Bruno <Bruno@seibtenterprises.com>
2026-03-07 12:23:11 -0500

    Update generate_function.py

--- generate_function.py
+++ generate_function.py
@@ -13,10 +13,25 @@ def count_vowels(text):
     return count
 
 
+def count_consonants(text):
+    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+    count = 0
+
+    for char in text:
+        if char in consonants:
+            count += 1
+
+    return count
+
+
 # User input
 user_text = input("Enter a string: ")
 
-# Function call
+# Function calls
 total_vowels = count_vowels(user_text)
+total_consonants = count_consonants(user_text)
 
 print("Number of vowels:", total_vowels)
+print("Number of consonants:", total_consonants)
+
+
